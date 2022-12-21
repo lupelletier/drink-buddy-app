46dfd9a173cdfc796019e02767a175681cb63ae4
luludegex06 <95019526+Luludegex06@users.noreply.github.com>
2022-12-21 13:46:54 +0100

    Updates

--- src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-import logo from '../img/logo-negatif.png';
-export default function Header() {
-  return (
-    <header className='separador'>
-      <img src={logo} alt="drink-budy-logo" className='logo'/>
-    </header>
-  );
-}
